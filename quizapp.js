ee6a0ff84452c74260d19cc26929ccfcbc2cd1f7
Luisa Salcedo <profe.salcedo@gmail.com>
2017-04-07 13:49:12 -0500

    set up string interpolation

--- quizapp.js
+++ quizapp.js
@@ -15,18 +15,41 @@ var state = {//single source of truth
 	}]
 }
 //Rendering
+//next two steps: 1)writing my html, 2) map it
+//will need to make the question number dynamic. 
 var renderList = function(state) {
 	var question = state.items[0]
 	var name = question.name
-	var warnings = name.warnings
+	var warnings = question.warnings
+	//item is always singular and index referring to which element with in the array
+	var itemsHtml = warnings.map(function(item,index){
+
+		//update classes
+		return `
+			<li index="${index}"> 
+        		<span class="">${item}</span>
+        		<div class="shopping-item-controls">
+        			<button class="shopping-item-toggle">
+        				<span class="button-label">check</span>
+        			</button>
+        			<button class="shopping-item-delete">
+        				<span class="button-label">delete</span>
+        			</button>
+        		</div>
+        	</li>
+	`
+	});
 
 	var html = `
-		<div>Hi</div>
+		<div>${name},${itemsHtml.join("")}</div>
 	`
 	$('.questions').html(html);
 }
 renderList(state)
 
+//set up event listeners 
+	//question/answer
+	//correct/incorrect response
 
 
  
