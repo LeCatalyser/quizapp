c3986756d7fdddb23657fd4d1d880122f73e8558
Luisa Salcedo <profe.salcedo@gmail.com>
2017-04-07 13:16:01 -0500

    named variables and linked html

--- quizapp.js
+++ quizapp.js
@@ -1,4 +1,38 @@
-//Questions: I will follow the MCV model.
+//Follow MCV model.
+//Model
+	//will contain variables inside the app.
 
+var state = {//single source of truth
+	items: [{
+		name: "Rosa Parks", 
+		warnings: [
+			"don't smile", 
+			"don't buy milk", 
+			"don't sit on the white side of the bus", 
+			"don't march"
+		],
+		result:2
+	}]
+}
+//Rendering
+var renderList = function(state) {
+	var question = state.items[0]
+	var name = question.name
+	var warnings = name.warnings
+
+	var html = `
+		<div>Hi</div>
+	`
+	$('.questions').html(html);
+}
+renderList(state)
+
+
+
+ 
+//Pieces on info per questions:
+	//1)"name" '+' "warning:" 
+	//2) multiple choice
+	//3) correct/incorrect response
 //quizz will load
 //question #1
